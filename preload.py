8d2856852ed02300a703f20bad0ccaefcb2a723d
yutian24130 <122241943+yutian24130@users.noreply.github.com>
2023-12-06 16:36:28 +0800

    Update preload.py

--- preload.py
+++ preload.py
@@ -1,3 +1,3 @@
 
 def preload(parser):
-    parser.add_argument("--encrypt-pass", type=str, help="The password to enable image encryption.", default=None)
+    parser.add_argument("--encrypt-pass1", type=str, help="The password to enable image encryption.", default=None)
